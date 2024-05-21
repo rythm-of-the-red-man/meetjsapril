00bfbf986359eb155ba46a5ae7645280d56a8f4d
Mateusz Pydych <mpydych@o2.pl>
2024-05-21 20:36:57 +0200

    new routes

--- server/index.mjs
+++ server/index.mjs
@@ -6,20 +6,20 @@ const port = 65534;
 const wsMain = expressWs(app);
 let ropeX = 0;
 
-app.post("/reset", (req, res) => {
+app.post("/app/reset", (req, res) => {
   ropeX = 0;
   for (let client of wsMain.getWss().clients) {
     client.send(ropeX);
   }
 });
-app.ws("/red", function (ws, req) {
+app.ws("/app/red", function (ws, req) {
   ws.on("message", function (msg) {
     ropeX += 1;
     ws.send(ropeX);
   });
 });
 
-app.ws("/blue", function (ws, req) {
+app.ws("/app/blue", function (ws, req) {
   ws.on("message", function (msg) {
     ropeX -= 1;
     for (let client of wsMain.getWss().clients) {
