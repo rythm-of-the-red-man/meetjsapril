289a4cc53e32ff0a58ba980a10585b81cfad68a5
Mateusz Pydych <mpydych@o2.pl>
2024-05-22 19:46:16 +0200

    credits

--- meetjs/src/App.tsx
+++ meetjs/src/App.tsx
@@ -5,27 +5,46 @@ import useWebSocket from "react-use-websocket";
 import { useEffect, useState } from "react";
 
 function App() {
+  // const apiBase = "localhost:65534";
+  const apiBase = "frog01.mikr.us:22077";
+  // Trzyma pozycje kamienia
+  const [position, setPosition] = useState(0);
+
+  /**
+   * Effect do ustawienia początkowej pozycji kamienia
+   * w razie refresha
+   */
+  useEffect(() => {
+    const setUp = async () => {
+      const requestOptions = {
+        method: "GET",
+      };
+      const newPosition = await fetch(
+        `http://${apiBase}/app/initial`,
+        requestOptions,
+      );
+      const position = parseInt((await newPosition.json()).position);
+      setPosition(position * 10);
+    };
+    setUp();
+  }, []);
+
+  // Dwa websockety pozwalające "ciągnąć" w obie strony.
   const {
     sendMessage: pullBlue,
     lastMessage: lastMessageBlue,
     readyState: readyStateBlue,
-  } = useWebSocket("ws://frog01.mikr.us:22077/app/blue");
+  } = useWebSocket(`ws://${apiBase}/app/blue`);
   const {
     sendMessage: pullRed,
     lastMessage: lastMessageRed,
     readyState: readyStateRed,
-  } = useWebSocket("ws://frog01.mikr.us:22077/app/red");
-  const [position, setPosition] = useState(0);
-  const reset = async () => {
-    const requestOptions = {
-      method: "POST",
-    };
-    await fetch("http://frog01.mikr.us:22077/app/reset", requestOptions);
-  };
+  } = useWebSocket(`ws://${apiBase}/app/red`);
+
+  // Effecty reagujące na wiadomości zwrotne z serwera
   useEffect(() => {
     setPosition(parseInt(lastMessageRed?.data || "0") * 10);
   }, [lastMessageRed]);
-
   useEffect(() => {
     setPosition(parseInt(lastMessageBlue?.data || "0") * 10);
   }, [lastMessageBlue]);
@@ -65,7 +84,17 @@ function App() {
           Pull Blue!
         </button>
       </div>
-      <button onClick={reset}>Reset</button>
+      <div
+        style={{ display: "flex", flexDirection: "column", marginTop: "50px" }}
+      >
+        credits:
+        <a href="https://istockphoto.com/pl/portfolio/George_Chairborn?mediatype=illustration">
+          Background
+        </a>
+        <a href="https://www.vecteezy.com/png/19527056-an-8-bit-retro-styled-pixel-art-illustration-of-a-stone-rock">
+          Stone
+        </a>
+      </div>
     </>
   );
 }
